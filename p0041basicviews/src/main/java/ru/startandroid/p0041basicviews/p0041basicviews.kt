eba59953a4f1da54c21cdeab74cf7db238191a2f
Mazaiken <grigorevsega@mail.ru>
2021-12-21 16:46:19 +0300

    initial commit

---
+++ p0041basicviews/src/main/java/ru/startandroid/p0041basicviews/p0041basicviews.kt
@@ -0,0 +1,11 @@
+package ru.startandroid.p0041basicviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class p0041basicviews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p0041basicviews)
+    }
+}
