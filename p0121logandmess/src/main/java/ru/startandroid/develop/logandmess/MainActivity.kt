3da579bbedcdbf232db47c485eaeb138cd73ebc0
Mazaiken <grigorevsega@mail.ru>
2022-02-17 17:39:38 +0300

    11.1урок

--- p0121logandmess/src/main/java/ru/startandroid/develop/logandmess/MainActivity.kt
+++ p0121logandmess/src/main/java/ru/startandroid/develop/logandmess/MainActivity.kt
@@ -1,11 +1,39 @@
-package ru.startandroid.develop.logandmess
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import ru.startandroid.develop.logandmess.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class MainActivity : Activity(), View.OnClickListener {
+    var tvOut: TextView? = null
+    var btnOk: Button? = null
+    var btnCancel: Button? = null
+
+    /** Called when the activity is first created.  */
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // найдем View-элементы
+        tvOut = findViewById<View>(R.id.tvOut) as TextView
+        btnOk = findViewById<View>(R.id.btnOk) as Button
+        btnCancel = findViewById<View>(R.id.btnCancel) as Button
+
+        // присваиваем обработчик кнопкам
+        btnOk?.setOnClickListener(this)
+        btnCancel?.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        // по id определяем кнопку, вызвавшую этот обработчик
+        when (v.id) {
+            R.id.btnOk ->       // кнопка ОК
+                tvOut!!.text = "Нажата кнопка ОК"
+            R.id.btnCancel ->       // кнопка Cancel
+                tvOut!!.text = "Нажата кнопка Cancel"
+        }
     }
 }
